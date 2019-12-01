70a4fc405a0cc904c9f49e22facb3cdc9d421ef5
kalleakerblom <kalleakerblom@gmail.com>
2019-12-01 09:54:36 +0100

    Set up repo and add day1

---
+++ src/day1.rs
@@ -0,0 +1,43 @@
+fn total_fuel_count(masses: &[u32]) -> u32 {
+    masses.iter().map(|m| m / 3 - 2).sum()
+}
+
+fn recursive_fuel_count(mass: u32) -> u32 {
+    if mass <= 2 * 3 {
+        return 0;
+    }
+    let count = mass / 3 - 2;
+    count + recursive_fuel_count(count)
+}
+fn total_recursive_fuel_count(masses: &[u32]) -> u32 {
+    masses.iter().map(|m| recursive_fuel_count(*m)).sum()
+}
+#[cfg(test)]
+mod tests {
+    use super::total_fuel_count;
+    use super::total_recursive_fuel_count;
+    use std::fs::File;
+    use std::io::{BufRead, BufReader};
+    #[test]
+    fn example_day1_part1() {
+        assert_eq!(total_fuel_count(&[100756]), 33583);
+    }
+    #[test]
+    fn day_1_part1() {
+        let mut buf = BufReader::new(File::open("input/day1").unwrap());
+        let masses: Vec<u32> =
+            buf.lines().map(|s| s.unwrap().parse::<u32>().unwrap()).collect();
+        assert_eq!(total_fuel_count(&masses), 3372463);
+    }
+    #[test]
+    fn example_day1_part2() {
+        assert_eq!(total_recursive_fuel_count(&[100756]), 50346);
+    }
+    #[test]
+    fn day_1_part2() {
+        let mut buf = BufReader::new(File::open("input/day1").unwrap());
+        let masses: Vec<u32> =
+            buf.lines().map(|s| s.unwrap().parse::<u32>().unwrap()).collect();
+        assert_eq!(total_recursive_fuel_count(&masses), 5055835);
+    }
+}
