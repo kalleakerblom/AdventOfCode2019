d19f5ef6ae0b5420ffbb01612bf2d56b5a0b7ed7
kalleakerblom <kalleakerblom@gmail.com>
2019-12-04 19:40:23 +0100

    Refactor day3: Fewer maps

--- src/day3.rs
+++ src/day3.rs
@@ -1,8 +1,7 @@
 use std::collections::HashMap;
-fn find_intersect_distances(wires: &[&str]) -> (u32, u32) {
-    let mut multi_wire_count: HashMap<(i32, i32), u32> = HashMap::new();
-    let mut multi_wire_steps: HashMap<(i32, i32), u32> = HashMap::new();
-    for wire in wires {
+fn find_intersect_distances(wire1: &str, wire2: &str) -> (u32, u32) {
+    let mut wire_maps = Vec::new();
+    for wire in &[wire1, wire2] {
         let mut pos = (0, 0);
         let mut wire_map = HashMap::new();
         wire_map.insert((0, 0), 0);
@@ -10,27 +9,23 @@ fn find_intersect_distances(wires: &[&str]) -> (u32, u32) {
         for instruction in wire.split(',') {
             draw_wire(instruction, &mut count, &mut wire_map, &mut pos);
         }
-        for &pos in wire_map.keys() {
-            *multi_wire_count.entry(pos).or_default() += 1;
-        }
-        for (pos, steps) in wire_map {
-            *multi_wire_steps.entry(pos).or_default() += steps;
-        }
+        wire_maps.push(wire_map);
     }
-
-    let ans1 = multi_wire_count
-        .iter()
-        .filter(|(&pos, &count)| count > 1 && pos != (0, 0))
-        .map(|((x, y), _)| x.abs() + y.abs())
-        .min()
-        .unwrap() as u32;
-    let ans2 = multi_wire_steps
-        .into_iter()
-        .filter(|&(pos, _)| multi_wire_count[&pos] > 1 && pos != (0, 0))
-        .map(|(_, steps)| steps)
+    let ans1 = wire_maps[0]
+        .keys()
+        .filter(|k| wire_maps[1].contains_key(k) && k != &&(0, 0))
+        .map(|(x, y)| x.abs() + y.abs())
         .min()
         .unwrap();
-    (ans1, ans2)
+    let ans2 = wire_maps[0]
+        .iter()
+        .filter(|(pos, _)| pos != &&(0, 0))
+        .filter_map(|(pos, steps)| {
+            wire_maps[1].get(pos).map(|other_steps| steps + other_steps)
+        })
+        .min()
+        .unwrap();
+    (ans1 as u32, ans2)
 }
 
 fn draw_wire(
@@ -78,19 +73,19 @@ mod tests {
     use std::fs;
     #[test]
     fn example_day3_part1_and_2() {
-        let ans = find_intersect_distances(&["R8,U5,L5,D3", "U7,R6,D4,L4"]);
+        let ans = find_intersect_distances("R8,U5,L5,D3", "U7,R6,D4,L4");
         assert_eq!(ans, (6, 30));
-        let ans = find_intersect_distances(&[
+        let ans = find_intersect_distances(
             "R75,D30,R83,U83,L12,D49,R71,U7,L72",
             "U62,R66,U55,R34,D71,R55,D58,R83",
-        ]);
+        );
         assert_eq!(ans, (159, 610));
     }
     #[test]
     fn day3_part1_and_2() {
         let input: String = fs::read_to_string("input/day3").unwrap();
         let lines: Vec<&str> = input.lines().collect();
-        let (ans1, ans2) = find_intersect_distances(&lines);
+        let (ans1, ans2) = find_intersect_distances(lines[0], lines[1]);
         assert_eq!(ans1, 352);
         assert_eq!(ans2, 43848);
     }
