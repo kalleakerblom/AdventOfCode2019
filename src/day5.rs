4cf266a67ad57a5f16b3e266a3ddf51349663883
kalleakerblom <kalleakerblom@gmail.com>
2019-12-05 22:30:15 +0100

    Make run_program return output, add tests and do some renaming of vars

--- src/day5.rs
+++ src/day5.rs
@@ -18,19 +18,20 @@ enum Op {
 
 fn run_program(mut code: Vec<i32>, input: i32) -> Vec<i32> {
     let mut head = 0;
+    let mut output = Vec::new();
     loop {
         let op = parse_op(&code, head);
-        let get_val = |p: Param| match p {
+        let value = |p: Param| match p {
             Param::Pos(pos) => code[pos],
             Param::Im(im) => im,
         };
         match op {
             Op::Add([p1, p2, Param::Pos(pos)]) => {
-                code[pos] = get_val(p1) + get_val(p2);
+                code[pos] = value(p1) + value(p2);
                 head += 4;
             }
             Op::Mul([p1, p2, Param::Pos(pos)]) => {
-                code[pos] = get_val(p1) * get_val(p2);
+                code[pos] = value(p1) * value(p2);
                 head += 4;
             }
             Op::In(Param::Pos(pos)) => {
@@ -38,79 +39,79 @@ fn run_program(mut code: Vec<i32>, input: i32) -> Vec<i32> {
                 head += 2;
             }
             Op::Out(param) => {
-                println!("out:{}", get_val(param));
+                output.push(value(param));
                 head += 2;
             }
             Op::JumpTrue([p1, p2]) => {
-                if get_val(p1) != 0 {
-                    head = get_val(p2) as usize;
+                if value(p1) != 0 {
+                    head = value(p2) as usize;
                 } else {
                     head += 3;
                 }
             }
             Op::JumpFalse([p1, p2]) => {
-                if get_val(p1) == 0 {
-                    head = get_val(p2) as usize;
+                if value(p1) == 0 {
+                    head = value(p2) as usize;
                 } else {
                     head += 3;
                 }
             }
             Op::Less([p1, p2, Param::Pos(pos)]) => {
-                code[pos] = if get_val(p1) < get_val(p2) { 1 } else { 0 };
+                code[pos] = if value(p1) < value(p2) { 1 } else { 0 };
                 head += 4;
             }
             Op::Equal([p1, p2, Param::Pos(pos)]) => {
-                code[pos] = if get_val(p1) == get_val(p2) { 1 } else { 0 };
+                code[pos] = if value(p1) == value(p2) { 1 } else { 0 };
                 head += 4;
             }
-            Op::Halt => return code,
+            Op::Halt => return output,
             _ => panic!("bad op"),
         }
     }
 }
 fn parse_op(code: &[i32], head: usize) -> Op {
     let op_code = code[head];
-    let DE = op_code % 100;
+    let de = op_code % 100;
     let op_code = op_code / 100;
-    let C = op_code % 10;
-    let B = (op_code / 10) % 10;
-    let A = (op_code / 100) % 10;
-    let build_param = |val, mode| match mode {
+    let c = op_code % 10;
+    let b = (op_code / 10) % 10;
+    let a = (op_code / 100) % 10;
+    let make_param = |val, mode| match mode {
         0 => Param::Pos(val as usize),
         1 => Param::Im(val),
         _ => panic!(),
     };
-    match DE {
+    match de {
         99 => Op::Halt,
         1 => Op::Add([
-            build_param(code[head + 1], C),
-            build_param(code[head + 2], B),
-            build_param(code[head + 3], A),
+            make_param(code[head + 1], c),
+            make_param(code[head + 2], b),
+            make_param(code[head + 3], a),
         ]),
         2 => Op::Mul([
-            build_param(code[head + 1], C),
-            build_param(code[head + 2], B),
-            build_param(code[head + 3], A),
+            make_param(code[head + 1], c),
+            make_param(code[head + 2], b),
+            make_param(code[head + 3], a),
         ]),
-        3 => Op::In(build_param(code[head + 1], C)),
-        4 => Op::Out(build_param(code[head + 1], C)),
+        3 => Op::In(make_param(code[head + 1], c)),
+        4 => Op::Out(make_param(code[head + 1], c)),
         5 => Op::JumpTrue([
-            build_param(code[head + 1], C),
-            build_param(code[head + 2], B),
+            make_param(code[head + 1], c),
+            make_param(code[head + 2], b),
         ]),
         6 => Op::JumpFalse([
-            build_param(code[head + 1], C),
-            build_param(code[head + 2], B),
+            make_param(code[head + 1], c),
+            make_param(code[head + 2], b),
         ]),
         7 => Op::Less([
-            build_param(code[head + 1], C),
-            build_param(code[head + 2], B),
-            build_param(code[head + 3], A),
+            make_param(code[head + 1], c),
+            make_param(code[head + 2], b),
+            make_param(code[head + 3], a),
         ]),
         8 => Op::Equal([
-            build_param(code[head + 1], C),
-            build_param(code[head + 2], B),
-            build_param(code[head + 3], A),
+            make_param(code[head + 1], c),
+            make_param(code[head + 2], b),
+            make_param(code[head + 3], a),
         ]),
 
         bad => panic!("bad op code ({}) at ({})", bad, head),
@@ -122,10 +123,7 @@ mod tests {
     use std::fs;
     #[test]
     fn example_day5_part1() {
-        assert_eq!(
-            run_program(vec![1002, 4, 3, 4, 33], 1),
-            vec![1002, 4, 3, 4, 99]
-        )
+        assert_eq!(run_program(vec![1002, 4, 3, 4, 33], 1), vec![])
     }
     #[test]
     fn day5_part1_and_2() {
@@ -136,8 +134,11 @@ mod tests {
             .map(|s| s.parse().unwrap())
             .collect();
         //part 1
-        run_program(program.clone(), 1);
+        assert_eq!(
+            run_program(program.clone(), 1),
+            vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 15259545]
+        );
         //part 2
-        run_program(program, 5);
+        assert_eq!(run_program(program.clone(), 5), vec![7616021]);
     }
 }
