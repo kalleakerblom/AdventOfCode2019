8881169232131bb0b5c58ad5d107e63862ef2b84
kalleakerblom <kalleakerblom@gmail.com>
2019-12-11 21:23:34 +0100

    Refactor day7 & day9 to use IntCode module

--- src/day7.rs
+++ src/day7.rs
@@ -1,150 +1,11 @@
-#[derive(Debug)]
-enum Param {
-    Pos(usize),
-    Rel(i32),
-    Im(i32),
-}
-#[derive(Debug)]
-enum Op {
-    Add([Param; 3]),
-    Mul([Param; 3]),
-    In(Param),
-    Out(Param),
-    JumpTrue([Param; 2]),
-    JumpFalse([Param; 2]),
-    Less([Param; 3]),
-    Equal([Param; 3]),
-    OffsetBase(Param),
-    Halt,
-}
-struct Program {
-    head: usize,
-    code: Vec<i32>,
-    base: usize,
-}
-impl Program {
-    fn new(head: usize, code: Vec<i32>, base: usize) -> Self {
-        Program { head, code, base }
-    }
-    fn run(&mut self, input: i32) -> Option<i32> {
-        let mut input = Some(input);
-        loop {
-            let op = parse_op(&self.code, self.head);
-            let value = |p: Param| match p {
-                Param::Pos(pos) => self.code[pos],
-                Param::Im(im) => im,
-                Param::Rel(rel) => {
-                    let pos = (self.base as i32 + rel) as usize;
-                    self.code[pos]
-                }
-            };
-            match op {
-                Op::Add([p1, p2, Param::Pos(pos)]) => {
-                    self.code[pos] = value(p1) + value(p2);
-                    self.head += 4;
-                }
-                Op::Mul([p1, p2, Param::Pos(pos)]) => {
-                    self.code[pos] = value(p1) * value(p2);
-                    self.head += 4;
-                }
-                Op::In(Param::Pos(pos)) => {
-                    self.code[pos] = input.take()?;
-                    self.head += 2;
-                }
-                Op::Out(param) => {
-                    let out = value(param);
-                    self.head += 2;
-                    return Some(out);
-                }
-                Op::JumpTrue([p1, p2]) => {
-                    if value(p1) != 0 {
-                        self.head = value(p2) as usize;
-                    } else {
-                        self.head += 3;
-                    }
-                }
-                Op::JumpFalse([p1, p2]) => {
-                    if value(p1) == 0 {
-                        self.head = value(p2) as usize;
-                    } else {
-                        self.head += 3;
-                    }
-                }
-                Op::Less([p1, p2, Param::Pos(pos)]) => {
-                    self.code[pos] = if value(p1) < value(p2) { 1 } else { 0 };
-                    self.head += 4;
-                }
-                Op::Equal([p1, p2, Param::Pos(pos)]) => {
-                    self.code[pos] = if value(p1) == value(p2) { 1 } else { 0 };
-                    self.head += 4;
-                }
-                Op::OffsetBase(param) => {
-                    let offset = value(param);
-                    self.base = (self.base as i32 + offset) as usize;
-                    self.head += 2;
-                }
-                Op::Halt => return None,
-                _ => panic!("bad op"),
-            }
-        }
-    }
-}
+use super::int_code::Program;
 
-fn parse_op(code: &[i32], head: usize) -> Op {
-    let op_code = code[head];
-    let de = op_code % 100;
-    let op_code = op_code / 100;
-    let c = op_code % 10;
-    let b = (op_code / 10) % 10;
-    let a = (op_code / 100) % 10;
-    let make_param = |val, mode| match mode {
-        0 => Param::Pos(val as usize),
-        1 => Param::Im(val),
-        2 => Param::Rel(val),
-        _ => panic!(),
-    };
-    match de {
-        99 => Op::Halt,
-        1 => Op::Add([
-            make_param(code[head + 1], c),
-            make_param(code[head + 2], b),
-            make_param(code[head + 3], a),
-        ]),
-        2 => Op::Mul([
-            make_param(code[head + 1], c),
-            make_param(code[head + 2], b),
-            make_param(code[head + 3], a),
-        ]),
-        3 => Op::In(make_param(code[head + 1], c)),
-        4 => Op::Out(make_param(code[head + 1], c)),
-        5 => Op::JumpTrue([
-            make_param(code[head + 1], c),
-            make_param(code[head + 2], b),
-        ]),
-        6 => Op::JumpFalse([
-            make_param(code[head + 1], c),
-            make_param(code[head + 2], b),
-        ]),
-        7 => Op::Less([
-            make_param(code[head + 1], c),
-            make_param(code[head + 2], b),
-            make_param(code[head + 3], a),
-        ]),
-        8 => Op::Equal([
-            make_param(code[head + 1], c),
-            make_param(code[head + 2], b),
-            make_param(code[head + 3], a),
-        ]),
-        9 => Op::OffsetBase(make_param(code[head + 1], c)),
-        bad => panic!("bad op code ({}) at ({})", bad, head),
-    }
-}
 fn run_program_chain(
-    mut input: i32,
+    mut input: i64,
     programs: &mut [&mut Program],
-) -> Option<i32> {
+) -> Option<i64> {
     for p in programs {
-        input = p.run(input)?;
+        input = p.run(Some(input))?;
     }
     Some(input)
 }
@@ -157,7 +18,7 @@ mod tests {
     use std::fs;
     #[test]
     fn day7_part1() {
-        let amp_code: Vec<i32> = fs::read_to_string("input/day7")
+        let amp_code: Vec<i64> = fs::read_to_string("input/day7")
             .unwrap()
             .trim()
             .split(',')
@@ -166,28 +27,28 @@ mod tests {
 
         let mut max_out = 0;
         for set in (0..5).permutations(5) {
-            let mut prog_a = Program::new(0, amp_code.clone(), 0);
-            let mut prog_b = Program::new(0, amp_code.clone(), 0);
-            let mut prog_c = Program::new(0, amp_code.clone(), 0);
-            let mut prog_d = Program::new(0, amp_code.clone(), 0);
-            let mut prog_e = Program::new(0, amp_code.clone(), 0);
-            prog_a.run(set[0]);
-            prog_b.run(set[1]);
-            prog_c.run(set[2]);
-            prog_d.run(set[3]);
-            prog_e.run(set[4]);
-            let out_a = prog_a.run(0).unwrap();
-            let out_b = prog_b.run(out_a).unwrap();
-            let out_c = prog_c.run(out_b).unwrap();
-            let out_d = prog_d.run(out_c).unwrap();
-            let out_e = prog_e.run(out_d).unwrap();
+            let mut prog_a = Program::new(amp_code.clone());
+            let mut prog_b = Program::new(amp_code.clone());
+            let mut prog_c = Program::new(amp_code.clone());
+            let mut prog_d = Program::new(amp_code.clone());
+            let mut prog_e = Program::new(amp_code.clone());
+            prog_a.run(Some(set[0]));
+            prog_b.run(Some(set[1]));
+            prog_c.run(Some(set[2]));
+            prog_d.run(Some(set[3]));
+            prog_e.run(Some(set[4]));
+            let out_a = prog_a.run(Some(0)).unwrap();
+            let out_b = prog_b.run(Some(out_a)).unwrap();
+            let out_c = prog_c.run(Some(out_b)).unwrap();
+            let out_d = prog_d.run(Some(out_c)).unwrap();
+            let out_e = prog_e.run(Some(out_d)).unwrap();
             max_out = cmp::max(max_out, out_e);
         }
         assert_eq!(max_out, 914_828);
     }
     #[test]
     fn day7_part2() {
-        let amp_code: Vec<i32> = fs::read_to_string("input/day7")
+        let amp_code: Vec<i64> = fs::read_to_string("input/day7")
             .unwrap()
             .trim()
             .split(',')
@@ -196,16 +57,16 @@ mod tests {
         let mut max_out = 0;
 
         for set in (5..10).permutations(5) {
-            let mut prog_a = Program::new(0, amp_code.clone(), 0);
-            let mut prog_b = Program::new(0, amp_code.clone(), 0);
-            let mut prog_c = Program::new(0, amp_code.clone(), 0);
-            let mut prog_d = Program::new(0, amp_code.clone(), 0);
-            let mut prog_e = Program::new(0, amp_code.clone(), 0);
-            prog_a.run(set[0]);
-            prog_b.run(set[1]);
-            prog_c.run(set[2]);
-            prog_d.run(set[3]);
-            prog_e.run(set[4]);
+            let mut prog_a = Program::new(amp_code.clone());
+            let mut prog_b = Program::new(amp_code.clone());
+            let mut prog_c = Program::new(amp_code.clone());
+            let mut prog_d = Program::new(amp_code.clone());
+            let mut prog_e = Program::new(amp_code.clone());
+            prog_a.run(Some(set[0]));
+            prog_b.run(Some(set[1]));
+            prog_c.run(Some(set[2]));
+            prog_d.run(Some(set[3]));
+            prog_e.run(Some(set[4]));
 
             let mut input = 0;
 
--- src/day9.rs
+++ src/day9.rs
@@ -1,157 +1,6 @@
-#[derive(Debug)]
-enum Param {
-    Pos(usize),
-    Rel(i64),
-    Im(i64),
-}
-#[derive(Debug)]
-enum Op {
-    Add([Param; 3]),
-    Mul([Param; 3]),
-    In(Param),
-    Out(Param),
-    JumpTrue([Param; 2]),
-    JumpFalse([Param; 2]),
-    Less([Param; 3]),
-    Equal([Param; 3]),
-    OffsetBase(Param),
-    Halt,
-}
-struct Program {
-    head: usize,
-    code: Vec<i64>,
-    base: usize,
-}
-impl Program {
-    fn new(code: Vec<i64>) -> Self {
-        Program { head: 0, code, base: 0 }
-    }
-    fn run(&mut self, input: i64) -> Option<i64> {
-        let mut input = Some(input);
-        loop {
-            let op = parse_op(&self.code, self.head);
-            let value = |p: Param| match p {
-                Param::Pos(pos) => self.code[pos],
-                Param::Im(im) => im,
-                Param::Rel(rel) => {
-                    let pos = (self.base as i64 + rel) as usize;
-                    self.code[pos]
-                }
-            };
-            let pos = |p: Param| match p {
-                Param::Pos(pos) => pos,
-                Param::Rel(rel) => (self.base as i64 + rel) as usize,
-                Param::Im(_) => panic!("immediate value invalid as pos"),
-            };
-            match op {
-                Op::Add([p1, p2, p3]) => {
-                    let pos = pos(p3);
-                    self.code[pos] = value(p1) + value(p2);
-                    self.head += 4;
-                }
-                Op::Mul([p1, p2, p3]) => {
-                    let pos = pos(p3);
-                    self.code[pos] = value(p1) * value(p2);
-                    self.head += 4;
-                }
-                Op::In(param) => {
-                    let pos = pos(param);
-                    self.code[pos] = input.take()?;
-                    self.head += 2;
-                }
-                Op::Out(param) => {
-                    let out = value(param);
-                    self.head += 2;
-                    return Some(out);
-                }
-                Op::JumpTrue([p1, p2]) => {
-                    if value(p1) != 0 {
-                        self.head = value(p2) as usize;
-                    } else {
-                        self.head += 3;
-                    }
-                }
-                Op::JumpFalse([p1, p2]) => {
-                    if value(p1) == 0 {
-                        self.head = value(p2) as usize;
-                    } else {
-                        self.head += 3;
-                    }
-                }
-                Op::Less([p1, p2, p3]) => {
-                    let pos = pos(p3);
-                    self.code[pos] = if value(p1) < value(p2) { 1 } else { 0 };
-                    self.head += 4;
-                }
-                Op::Equal([p1, p2, p3]) => {
-                    let pos = pos(p3);
-                    self.code[pos] = if value(p1) == value(p2) { 1 } else { 0 };
-                    self.head += 4;
-                }
-                Op::OffsetBase(param) => {
-                    let offset = value(param);
-                    self.base = (self.base as i64 + offset) as usize;
-                    self.head += 2;
-                }
-                Op::Halt => return None,
-            }
-        }
-    }
-}
-
-fn parse_op(code: &[i64], head: usize) -> Op {
-    let op_code = code[head];
-    let de = op_code % 100;
-    let op_code = op_code / 100;
-    let c = op_code % 10;
-    let b = (op_code / 10) % 10;
-    let a = (op_code / 100) % 10;
-    let make_param = |val, mode| match mode {
-        0 => Param::Pos(val as usize),
-        1 => Param::Im(val),
-        2 => Param::Rel(val),
-        _ => panic!(),
-    };
-    match de {
-        99 => Op::Halt,
-        1 => Op::Add([
-            make_param(code[head + 1], c),
-            make_param(code[head + 2], b),
-            make_param(code[head + 3], a),
-        ]),
-        2 => Op::Mul([
-            make_param(code[head + 1], c),
-            make_param(code[head + 2], b),
-            make_param(code[head + 3], a),
-        ]),
-        3 => Op::In(make_param(code[head + 1], c)),
-        4 => Op::Out(make_param(code[head + 1], c)),
-        5 => Op::JumpTrue([
-            make_param(code[head + 1], c),
-            make_param(code[head + 2], b),
-        ]),
-        6 => Op::JumpFalse([
-            make_param(code[head + 1], c),
-            make_param(code[head + 2], b),
-        ]),
-        7 => Op::Less([
-            make_param(code[head + 1], c),
-            make_param(code[head + 2], b),
-            make_param(code[head + 3], a),
-        ]),
-        8 => Op::Equal([
-            make_param(code[head + 1], c),
-            make_param(code[head + 2], b),
-            make_param(code[head + 3], a),
-        ]),
-        9 => Op::OffsetBase(make_param(code[head + 1], c)),
-        bad => panic!("bad op code ({}) at ({})", bad, head),
-    }
-}
-
 #[cfg(test)]
 mod tests {
-    use super::Program;
+    use crate::int_code::Program;
     use std::fs;
     use std::iter;
     #[test]
@@ -163,7 +12,7 @@ mod tests {
                 .collect();
         code.extend(iter::repeat(0).take(100));
         let mut prog = Program::new(code);
-        while let Some(out) = prog.run(0) {
+        while let Some(out) = prog.run(None) {
             println!("{:?}", out)
         }
 
@@ -173,7 +22,7 @@ mod tests {
             .collect();
         code.extend(iter::repeat(0).take(100));
         let mut prog = Program::new(code);
-        assert_eq!(prog.run(0), Some(1_219_070_632_396_864));
+        assert_eq!(prog.run(None), Some(1_219_070_632_396_864));
 
         let mut code: Vec<i64> = "104,1125899906842624,99"
             .split(',')
@@ -181,7 +30,7 @@ mod tests {
             .collect();
         code.extend(iter::repeat(0).take(100));
         let mut prog = Program::new(code);
-        assert_eq!(prog.run(0), Some(1_125_899_906_842_624));
+        assert_eq!(prog.run(None), Some(1_125_899_906_842_624));
     }
     #[test]
     fn day9_part1_and_2() {
@@ -194,9 +43,9 @@ mod tests {
         code.extend(iter::repeat(0).take(1000));
         // part 1
         let mut prog = Program::new(code.clone());
-        assert_eq!(prog.run(1), Some(3_742_852_857));
+        assert_eq!(prog.run(Some(1)), Some(3_742_852_857));
         // part 2
         let mut prog_2 = Program::new(code);
-        assert_eq!(prog_2.run(2), Some(73439));
+        assert_eq!(prog_2.run(Some(2)), Some(73439));
     }
 }
